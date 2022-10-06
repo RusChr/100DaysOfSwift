30bcb44d95d3ab85bdffe95771e0f803c80eeb4f
Rus Chr <r.chergizbiev@gmail.com>
2022-10-06 17:39:43 +0300

    Project16: done

---
+++ Project16/Project16/Capital.swift
@@ -0,0 +1,23 @@
+//
+//  Capital.swift
+//  Project16
+//
+//  Created by Rustam Chergizbiev on 10/6/22.
+//
+
+import MapKit
+import UIKit
+
+class Capital: NSObject, MKAnnotation {
+	
+	var title: String?
+	var coordinate: CLLocationCoordinate2D
+	var info: String
+	
+	init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+		self.title = title
+		self.coordinate = coordinate
+		self.info = info
+	}
+
+}
